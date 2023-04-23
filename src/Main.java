2919b4166b9b1896ac18d63af459037a5cec1a52
Do Thien Hoang <theksbd@gmail.com>
2023-04-23 18:18:33 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,19 @@
 public class Main {
-    private String name;
-    private int age;
-    private String id;
-
-    public Main(String name, int age, String id) {
-        this.name = name;
-        this.age = age;
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Student student = new Student("John", "123", 20);
+
+        System.out.println(student.getName());
+        System.out.println(student.getId());
+        System.out.println(student.getAge());
+
+        student.setName("John Doe");
+        student.setId("456");
+        student.setAge(21);
+
+        System.out.println(student.getName());
+        System.out.println(student.getId());
+        System.out.println(student.getAge());
     }
 }
